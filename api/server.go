df20e8599301e968003bb1f4eef0afd063155bad
Christian <65543136+Makuo12@users.noreply.github.com>
2024-02-22 12:38:55 +0100

    change photo from media to name

--- api/server.go
+++ api/server.go
@@ -314,10 +314,10 @@ func NewServer(config utils.Config, store *db.SQLStore) (*Server, error) {
 		log.Printf(" Error at cron at job.AddFunc for HandleEventExperienceToRedis  %v", err.Error())
 	}
 
-	_, err = job.AddFunc("@every 2m", HandleImageMetaData(ctx, server))
-	if err != nil {
-		log.Printf(" Error at cron at job.AddFunc for HandleImageMetaData  %v", err.Error())
-	}
+	//_, err = job.AddFunc("@every 2m", HandleImageMetaData(ctx, server))
+	//if err != nil {
+	//	log.Printf(" Error at cron at job.AddFunc for HandleImageMetaData  %v", err.Error())
+	//}
 
 	// Start the cron scheduler
 	job.Start()
